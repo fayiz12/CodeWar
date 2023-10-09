fd0d7b5b2f390d3c5dea6042eea3adcb88a4eff1
Fayiz <77289134+fayiz12@users.noreply.github.com>
2023-10-09 22:15:26 +0530

    Create vowelCount.py

---
+++ vowelCount.py
@@ -0,0 +1,9 @@
+def get_count(sentence):
+    count=0
+    vowels='aeiou'
+    for i in sentence:
+        for j in vowels:
+            if i==j:
+                count=count+1
+    
+    return count
